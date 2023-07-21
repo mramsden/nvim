8570efd831bf83bf32e119eeaed3528c8c3e5c6a
Marcus Ramsden <marcus@bitsden.com>
2023-07-21 18:29:38 +0100

    Add markdown support

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup({
-  ensure_installed = { 'javascript', 'typescript', 'lua', 'vim', 'vimdoc', 'query', 'sql' },
+  ensure_installed = { 'javascript', 'typescript', 'lua', 'markdown', 'vim', 'vimdoc', 'query', 'sql' },
   sync_install = false,
   auto_install = true,
   highlight = {
